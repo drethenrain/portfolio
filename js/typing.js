82ac241f9c5561fb92d061b8fc959f131a679469
drethenrain <drethenrain@hotmail.com>
2021-07-27 09:57:36 -0300

    ✨ first commit

---
+++ js/typing.js
@@ -0,0 +1,10 @@
+new Typewriter('#typewriter', {
+  strings: [
+    'Back-end Developer',
+    'React Developer',
+    'Simple UI Designer',
+    'JavaScript Lover',
+  ],
+  autoStart: true,
+  loop: true,
+});
