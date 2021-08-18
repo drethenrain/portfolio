4134f7e893a38636a988666fa0f0606328667636
drethenrain <drethenrain@hotmail.com>
2021-08-18 20:29:42 -0300

    more luangameplays

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Typewriter } from 'react-simple-typewriter';
 
 import { Navbar } from '../components/Navbar';
 import { Footer } from '../components/Footer';
 
-import audio from '../assets/luangameplays.mp3';
+import luan1 from '../assets/luangameplays.mp3';
+import luan2 from '../assets/luangameplays2.mp3';
 
 export function Home() {
-  const [clicks, setClicks] = useState(0);
-  if (clicks === 30)
-    return (
-      <audio autoPlay>
-        <source src={audio} type="audio/mp3" />
-      </audio>
-    );
+  const [clicksLuan1, setClicksLuan1] = useState(0);
+  const [clicksLuan2, setClicksLuan2] = useState(0);
+
+  useEffect(() => {
+    if (clicksLuan1 === 30) {
+      document.getElementById('luan1').play();
+    }
+
+    if (clicksLuan2 === 30) {
+      document.getElementById('luan2').play();
+    }
+  });
 
   return (
     <>
@@ -21,18 +27,24 @@ export function Home() {
       <main className="home">
         <div>
           <div id="avatar-div">
-            <img
-              id="avatar"
-              src="https://avatars.githubusercontent.com/u/76497268?v=4"
-              alt="avatar"
-            />
+            <span
+              onClick={() => {
+                setClicksLuan2(clicksLuan2 + 1);
+              }}
+            >
+              <img
+                id="avatar"
+                src="https://avatars.githubusercontent.com/u/76497268?v=4"
+                alt="avatar"
+              />
+            </span>
           </div>
           <h2 className="title text-center">Hi 👋, I'm Lucas</h2>
           <p className="subtitle">
             A person who likes to contribute.
             <span
               onClick={() => {
-                setClicks(clicks + 1);
+                setClicksLuan1(clicksLuan1 + 1);
               }}
             >
               <img
@@ -52,6 +64,12 @@ export function Home() {
               />
             </span>
           </p>
+          <audio id="luan1">
+            <source src={luan1} type="audio/mp3" />
+          </audio>
+          <audio id="luan2">
+            <source src={luan2} type="audio/mp3" />
+          </audio>
         </div>
       </main>
       <Footer />
